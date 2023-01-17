a295df76d928c188d768e8ef2a20124f0d59ae0e
Renan Almeida <101940943+RenanMaestrya@users.noreply.github.com>
2023-01-17 18:32:45 -0300

    bot em puppeteer

--- learningPuppeteer/index.js
+++ learningPuppeteer/index.js
@@ -1,17 +1,27 @@
-const puppeteer =  require('puppeteer')
-console.log('Bem vindo');
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
+
+const puppeteer =  require('puppeteer');
+
+const site = process.env.SITE;
+const email = process.env.EMAIL;
+const senha = process.env.PASSWORD;
+
 
 async function robo(){
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-  
-    await page.goto('https://zplay.serveript.com/index.php?exe=logoff');
-    await page.type('[name="username"]', 'renanalmeida2.ra@gmail.com.br');
-    await page.type('[name="password"]', 'Mancoso200400');
-    await page.click('[type="submit"]');
 
-
-    // await page.click('#vertical-menu-btn');
+    try{
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+      
+        await page.goto(site);
+        await page.type('[name="username"]', email);
+        await page.type('[name="password"]', senha);
+        await page.click('[type="submit"]');
+    } catch(error){
+        console.log(error);
+    }
 }
 
 robo();
